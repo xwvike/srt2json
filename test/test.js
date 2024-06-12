1a76c100ece027d04a77e780e9d81a94fd45bdbd
xwvike <xwvike@gmail.com>
2024-06-12 17:57:16 +0800

    init

--- test/test.js
+++ test/test.js
@@ -1,7 +1,7 @@
-const str2json = require('../index.js');
+const srt2json = require('../index.js');
 const fs = require('fs');
 const path = require('path');
 
 const file = fs.readFileSync(path.resolve(__dirname, './test.srt'), 'utf-8');
 
-console.log(str2json(file));
+console.log(srt2json(file));
